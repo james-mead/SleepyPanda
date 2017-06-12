6b0002fe21e6d092c58e21ad45332607dba1b18a
james-mead <james.luke.mead@gmail.com>
2017-06-12 16:13:35 +1200

    added different sounds

--- __tests__/components.js
+++ __tests__/components.js
@@ -1,33 +1,13 @@
-import 'react-native'
 import React from 'react'
+import {
+  Text,
+  View,
+  ListView
+} from 'react-native'
 import renderer from 'react-test-renderer'
-// import { shallow } from 'enzyme'
 
 import Header from '../src/components/Header.js'
 import Player from '../src/components/Player.js'
-// import SoundList from '../src/components/SoundList.js'
-
-// function MyComponent() {
-//   return <div />
-// }
-//
-// const renderer = createRenderer()
-//
-// const tree = renderer.render(<MyComponent />)
-// expect(isDOMComponent(tree)).toBe(true)
-
-// describe('Foo component', function() {
-//   it('renders 3 Image components with the right props', function() {
-//     const renderer = TestUtils.createRenderer()
-//     renderer.render(<Header />)
-//     const result = renderer.getRenderOutput()
-//     const sources = map(findAllWithType(result, Image),
-//                         'props.source')
-//     expect(sources).toDeepEqual([
-//       '../images/Logo.png'
-//     ])
-//   })
-// })
 
 describe('Header (Snapshot)', () => {
   it('Renders the Header Component', () => {
@@ -48,8 +28,6 @@ describe('Player (Snapshot)', () => {
 describe('Confirms Sound Data is loaded', () => {
   it('renders the name of each sound file', () => {
   const soundData = require('../src/data/sounds')
-  const ListView = require('ListView')
-  const Text = require('Text')
   const dataSource = new ListView.DataSource({
     rowHasChanged: (r1, r2) => r1 !== r2,
   }).cloneWithRows(soundData)
@@ -62,5 +40,5 @@ describe('Confirms Sound Data is loaded', () => {
     )
     const json = component.toJSON()
   expect(component).toMatchSnapshot()
-})
+  })
 })
